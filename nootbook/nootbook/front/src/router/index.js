7b5571a9a0ccc606baa347ca8b5c163917ad57cd
dashuaibi <1275662488@qq.com>
2019-12-03 22:23:37 +0800

    nootbook笔记本

---
+++ nootbook/nootbook/front/src/router/index.js
@@ -0,0 +1,47 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import StarNotes from '@/components/StarNotes'
+import StarBanner from '@/components/starBanner/StarBanner'
+import StarLogin from '@/components/login/StarLogin'
+import StarRegister from '@/components/register/StarRegister'
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'StarNotes',
+      component: StarNotes,
+      meta: {
+        title: '星辰笔记'
+      }
+    },
+    {
+      path: '/StarBanner',
+      name: 'StarBanner',
+      component: StarBanner,
+      meta: {
+        title: '欢迎'
+      },
+      
+    },
+    {
+      path: '/StarLogin',
+      name: 'StarLogin',
+      component: StarLogin,
+      meta: {
+        title: '登录'
+      },
+      
+    },
+    {
+      path: '/StarRegister',
+      name: 'StarRegister',
+      component: StarRegister,
+      meta: {
+        title: '注册'
+      },
+      
+    }
+  ]
+})
---
+++ nootbook/nootbook/noot_book/controllers/defaultConfig.js
@@ -0,0 +1,12 @@
+const config = {
+  database: {
+    DATABASE: 'test',
+    USERNAME: 'root',
+    PASSWORD: '123456',//xfwnxn1023
+    PORT: '3306',
+    HOST: 'localhost'//192.168.31.25
+  }
+}
+
+
+module.exports = config
