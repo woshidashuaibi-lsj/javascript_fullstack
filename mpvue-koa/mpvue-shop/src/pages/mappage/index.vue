ecd568b3a51b00156cf15bab6882afda0e3ed0a2
dashuaibi <1275662488@qq.com>
2020-02-01 23:10:11 +0800

    全栈学习3

---
+++ mpvue-koa/mpvue-shop/src/pages/mappage/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="mappage">
+      <div class="section">
+          <input type="text" placeholder="搜索" focus="true" >
+      </div>
+      <scroll-view :scroll-y="true" class="addcont" style="height: 500rpx">
+          <div class="result" v-for="(item,index) in tips" :key="index">
+              {{item.name}}
+          </div>
+      </scroll-view>
+
+      <div class="map_container">
+          <div class="title">显示当前位置:</div>
+          <map class="map" id="map" scale="16" :longitude="longitude" :latitude="latitude" :markers="markers"></map>
+      </div>
+  </div>
+</template>
+
+<script>
+import amapFile from '../../utils/amap-wx.js'
+export default {
+data(){
+    return{
+        tips:[],
+        longitude: 0,
+        latitude: 0,
+        markers: []
+    }
+},
+mounted(){
+    this.getMapaddress()
+},
+methods:{
+    getMapaddress(){
+        let _this = this
+        var myAmapFun = new amapFile.AMapWX({key:'a92097f46362af3b30cf10af699fd50d'})
+        myAmapFun.getRegeo({
+            iconPath:"/static/images/marker.png",
+            iconWidth:22,
+            iconHeight:32,
+            success: function(data){
+                console.log(data)
+            },
+            fail(info){
+                console.log(info)
+            }
+        })
+    }
+}
+}
+</script>
+
+<style lang="less" scoped>
+@import "./style.less";
+</style>
