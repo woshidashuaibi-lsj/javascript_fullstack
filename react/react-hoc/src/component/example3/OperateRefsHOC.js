86fc8fc522d5bb908f435074a2d2b42536256aec
dashuaibi <1275662488@qq.com>
2020-03-05 21:58:19 +0800

    react 的高阶组件

---
+++ react/react-hoc/src/component/example3/OperateRefsHOC.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function HOC(WrapperComponent) {
+    let inputElement = null
+    function handleClick () {
+        inputElement.focus()
+    }
+
+    return (props) => (
+        <div>
+            <WrapperComponent inputRef={(el) => { inputElement= el}} {...props} />
+            <input type="Button" value="获取子组件输入框焦点" onClick={handleClick}/>
+        </div>
+    )
+}
+
+export default HOC
--- react/react-hoc/src/index.js
+++ react/react-hoc/src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './component/example1';
-import App from './component/example2';
+// import App from './component/example2';
+import App from './component/example3';
 import * as serviceWorker from './serviceWorker';
 
 // ReactDOM.render(<App />, document.getElementById('root'));
