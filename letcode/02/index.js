7dc3805cc3ec5471bbcd5bca0b57a0df15be47b2
dashuaibi <1275662488@qq.com>
2020-04-13 00:14:01 +0800

    算法数组排序（链表）

---
+++ letcode/02/index.js
@@ -0,0 +1,124 @@
+// 链表节点
+class Node {
+    constructor(element) {
+        this.element =element
+        this.next=null
+    }
+}
+
+//链表
+// let list = new LinkedList()
+
+
+
+class LinkedList {
+   constructor() {
+       this.head = null
+       this.length = 0
+   }
+
+//    追加元素
+append(element) {
+    const node = new Node(element)
+    let current = null
+    if(this.head === null){
+        this.head = node
+    } else {
+        current = this.head
+       while(current.next) {
+           current = current.next
+       }
+       current.next = node
+    }
+    this.length++
+}
+
+//在任意位置插入元素
+insert(position,element) {
+    if(position>=0 && position <=this.length) {
+        const node = new Node(element)
+        let current = this.head
+        let previous = null
+        let index = 0
+        if(position === 0) {
+            this.head= node
+        }else {
+            while(index++ < position){
+                previous = current
+                current = current.next
+            }
+            node.next = current
+            previous.next = node
+        }
+        this.length++
+        return true
+    }
+return false
+}
+
+
+// 移除指定位置元素
+   removeAt (position) {
+    //    检查越界值
+    if(position>-1 && position<length){
+        let current = this.head
+        let previous = null
+        let index =0
+        if(position===0) {
+            this.head=current.next
+        }
+        else{
+            while(index++ < position) {
+                previous = current
+                current =current.next
+            }
+            previous.next = current.next
+        }
+        this.length--
+        return current.element
+    }
+    return null
+   }
+
+   // 寻找元素下标
+findIndex(element) {
+    let current = this.head
+    let index = -1
+    while(current){
+        if(element === current.element){
+            return index + 1
+        }
+        index++
+        current = current.next
+    }
+    return -1
+}
+
+// 删除指定的文档
+remove (element) {
+    const index = this.indexOf(element)
+    return this.removeAt(index)
+}
+
+
+isEmpty(){
+    return !this.length
+}
+
+
+size() {
+    return this.length
+}
+
+// 转为字符串
+toString() {
+    let current =this.head
+    let string = ''
+    while(current){
+        string += `${current.element}`
+        current =current.next
+    }
+    return string
+}
+}
+
